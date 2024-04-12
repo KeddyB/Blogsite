0e75308c8b3f2c42098496301f40b7671a148c8b
Keddy <bokelusi@gmail.com>
2024-04-13 00:55:10 +0100

    this is the first commit for this day and i must say a lot of progress has been made

---
+++ blog/app.js
@@ -0,0 +1,65 @@
+//smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// sanity backend
+const urlParams = new URLSearchParams(window.location.search);
+const slug = urlParams.get('slug');
+
+let PROJECT_ID = "6kl7o5z0"
+let DATASET_NAME = "production"
+let QUERY = encodeURIComponent(`*[_type == "post" && slug.current =="${slug}"]
+{
+  title,
+  author->{
+    name
+  },
+  _updatedAt,
+  mainImage{
+    asset->{
+      url
+    }
+  },
+  body[0]{
+    children[0]{
+      text
+    }
+  }
+}
+`)
+let URL = `https://${PROJECT_ID}.api.sanity.io/v2022-03-07/data/query/${DATASET_NAME}?query=${QUERY}`
+
+fetch(URL)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    // Extract the blog post from the data
+    const blogPost = data.result[0];
+    // Populate the template with the fetched content
+    const blogPostContent = document.querySelector('.mainBlog');
+    blogPostContent.innerHTML = `
+        <h1>${blogPost.title}</h1>
+        <p class="author">Author: ${blogPost.author.name}</p>
+        <p class="date">Last Updated: ${blogPost._updatedAt}</p>
+        <img class="postImage" src="${blogPost.mainImage.asset.url}" alt="Main Image">
+        <p class="postText">${blogPost.body.children.text}</p>
+    `;
+  })
+  .catch(error => {
+      console.error('Error fetching blog post:', error);
+      // Optionally display an error message or handle the error in another way
+  })
+
+//current year on footer
+// function getCurrentYear() {
+//   return new Date().getFullYear();
+// }
+
+// document.querySelector(".year").innerHTML =`${getCurrentYear()}`
